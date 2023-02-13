1399f51bc42513c1cf4c9abadf77fec597e35cda
Malaica Brunks <malaica.brunks@gmx.de>
2023-02-13 11:41:49 +0100

    shuffle card-stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,19 +9,28 @@ import { Game } from 'src/models/game';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string = '';
   game: Game;
 
   constructor() { }
 
   ngOnInit(): void {
-
+    this.newGame();
   }
 
   newGame() {
     this.game = new Game();
+    console.log(this.game);
   }
 
   takeCard() {
+    if (!this.pickCardAnimation) {
+    this.currentCard = this.game.stack.pop();
+    console.log(this.currentCard);
     this.pickCardAnimation = true;
-  }
+
+    setTimeout(() => {
+      this.pickCardAnimation = false;
+    }, 1500);
+  }}
 }
