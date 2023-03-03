6022279d448a3345f8f0736c1b361c550b20b5a5
Malaica Brunks <malaica.brunks@gmx.de>
2023-03-03 09:37:17 +0100

    added angularfire

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-87302',
+    appId: '1:770953654798:web:d205569a1f742d07a0c25f',
+    storageBucket: 'ring-of-fire-87302.appspot.com',
+    apiKey: 'AIzaSyC0dvFqt4rZbOa-Bz-GW29ZMQiLRg_6HcU',
+    authDomain: 'ring-of-fire-87302.firebaseapp.com',
+    messagingSenderId: '770953654798',
+  },};
